f1c6899afaff274fd836700efb078b5e9b90583a
rudolif <rudolifrudolif@gmail.com>
2018-12-04 19:30:09 +0200

    commit

---
+++ app/js/common.js
@@ -0,0 +1,121 @@
+$(function() {
+
+new WOW().init();
+
+//----------------------акардион-------------------------------------------------
+  $(".customer__item_title").on("click", function(){
+    if ($(this).parent().hasClass('active')) {
+      $(this).parent().removeClass('active');
+      $(".customer__item_content").slideUp("slow");
+    }
+    else {
+      $(".active .customer__item_content").slideUp("slow");
+      $(".active").removeClass('active');
+
+      $(this).parent().addClass('active');
+      $(".active .customer__item_content").slideDown("slow");
+    }
+  });
+
+
+//------------------------------слайдер-----------------------------
+var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+//------------------------------гамбургер-----------------------------
+  $('.hamburger').click(function() {
+    $(this).toggleClass('hamburger--active');
+    $('.nav-bar').toggleClass('nav-bar--active');
+    $('.header').toggleClass('header--menu');
+  });
+
+//-------------------------------попандер---------------------------------------
+  $('.modal').popup({transition: 'all 0.3s'});
+
+//------------------------------------form-------------------------------------------
+  $('input[type="tel"]').mask('+0 (000) 000-00-00');
+
+  jQuery.validator.addMethod("phoneno", function(phone_number, element) {
+     return this.optional(element) || phone_number.match(/\+[0-9]{1}\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}/);
+  }, "Введите Ваш телефон");
+
+  $(".form").each(function(index, el) {
+    $(el).addClass('form-' + index);
+
+    $('.form-' + index).validate({
+      rules: {
+        phone: {
+          required: true,
+          phoneno: true
+        },
+        name: 'required',
+      },
+      messages: {
+        name: "Введите Ваше имя",
+        phone: "Введите Ваш телефон",
+      },
+      submitHandler: function(form) {
+        var t = {
+          name: jQuery('.form-' + index).find("input[name=name]").val(),
+          phone: jQuery('.form-' + index).find("input[name=phone]").val(),
+          question: jQuery('.form-' + index).find("textarea[name=question]").val(),
+          subject: jQuery('.form-' + index).find("input[name=subject]").val()
+        };
+        ajaxSend('.form-' + index, t);
+      }
+    });
+
+  });
+
+  function ajaxSend(formName, data) {
+    jQuery.ajax({
+      type: "POST",
+      url: "sendmail.php",
+      data: data,
+      success: function() {
+        $(".modal").popup("hide");
+        $("#thanks").popup("show");
+        setTimeout(function() {
+          $(formName).trigger('reset');
+        }, 2000);
+      }
+    });
+  }
+
+//----------------------------------------fixed----------------------------------
+  $(window).scroll(function(){
+      if($(this).scrollTop()>20){
+          $('.nav-bar').addClass('nav-bar--top');
+      }
+      else if ($(this).scrollTop()<20){
+          $('.nav-bar').removeClass('nav-bar--top');
+      }
+  });
+
+//-------------------------скорость якоря---------------------------------------
+  $(".nav").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top - 60}, 'slow', 'swing');
+  //--------------------закриття меню при кліку на ссилку якоря--------------------
+     // $('.hamburger').removeClass('hamburger--active');
+     // $('.header-menu').removeClass('header-menu');
+     // $('.header--active').removeClass('header--active');
+     // $('.nav--active').removeClass('nav--active');
+
+  });
+
+});
+
+//----------------------------------------preloader----------------------------------
+
+  $(window).on('load', function(){
+    $('.preloader').delay(1000).fadeOut('slow');
+  });
+
